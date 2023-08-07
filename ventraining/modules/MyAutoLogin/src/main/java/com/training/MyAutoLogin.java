52b843314d5e988a61a5618dc808f767e0b1b87f
Anand Shankar <ot@ashankar.com>
2023-08-07 09:25:24 +0530

    Updated.

--- ventraining/modules/MyAutoLogin/src/main/java/com/training/MyAutoLogin.java
+++ ventraining/modules/MyAutoLogin/src/main/java/com/training/MyAutoLogin.java
@@ -8,7 +8,7 @@ import org.osgi.service.component.annotations.Component;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.security.auto.login.AutoLogin;
 import com.liferay.portal.kernel.security.auto.login.AutoLoginException;
-import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
 
 /**
  * @author Anand
@@ -28,29 +28,65 @@ public class MyAutoLogin implements AutoLogin {
 	@Override
 	public String[] login(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
 			throws AutoLoginException {
-
 		
-		String[] s=new String[] {"user name", "password", Boolean.FALSE.toString()};
-
-		System.out.println("Hello Auto Login>>>>>>>>>>>>>>>>>>>>>>>");
 		
+		System.out.println(">>>>>>>>>>>>>>>>>>>> Hello AutoLogin");
+
+		String[] s=null;
 		
 try {
-			
-//			User user=PortalUtil.getUser(httpServletRequest);
-			
-			 User user= (User)httpServletRequest.getAttribute(WebKeys.USER);
-//		
-//			 
-			 if(user != null)
-			 {
-				 System.out.println(user.getEmailAddress());
-				 
-			 }else {
-				
-				 System.out.println(">>>>>>>>>>>>>>>>>>>> User is null");
-			}
+	
+	
+//	String username=httpServletRequest.getParameter("_com_liferay_login_web_portlet_LoginPortlet_login");
+//	String pass=httpServletRequest.getParameter("_com_liferay_login_web_portlet_LoginPortlet_password");
+	
+	String emailId=httpServletRequest.getParameter("emailId");
+	String pass=httpServletRequest.getParameter("pass");
+	String userId=null;
+	
+	
+	System.out.println(emailId);
+	System.out.println(pass);
+	
+//			System.out.println(">>>>>>>>>>>>>>>>>>>>> before");
+//			//Company company=PortalUtil.getCompany(httpServletRequest);
+//			System.out.println(">>>>>>>>>>>>>>>>>>>>> after");
 			 
+			// System.out.println(">>>>>>>>>>>>>>>>>>>>> company id:" + company.getCompanyId());
+			 
+	
+			User user=UserLocalServiceUtil.getUserByEmailAddress(20101l, emailId);
+			
+			if(user !=null)
+			{
+				
+				userId=String.valueOf(user.getUserId());
+				System.out.println(">>>>>>>>>>>>>>>>>>>>> user id: " + userId);
+				
+				
+			}else {
+				
+				System.out.println(">>>>>>>>>>>>>>>>>>>>> user is null:");
+			}
+
+			
+			
+	
+//		PortalUtil.getUser
+			
+			// User user= (User)httpServletRequest.getAttribute(WebKeys.USER);
+			 
+			// System.out.println(user.getUserId()); 
+		
+//			 if(user != null)
+//			 {
+//				 System.out.println(user.getEmailAddress());
+//				 
+//			 }else {
+//				
+//				 System.out.println(">>>>>>>>>>>>>>>>>>>> User is null");
+//			}
+			s=new String[] {userId, pass, Boolean.FALSE.toString()};	 
 			 
 		} catch (Exception e) {
 			e.printStackTrace();
